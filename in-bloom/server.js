35a20e1e07b7611f4ff65e3abfab3bc8eae3173d
Josh Wilson <joshua.wilson6289@gmail.com>
2020-12-10 21:26:22 -0600

    adding server.js file, along with models and routes folders

---
+++ in-bloom/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Requires models folder for syncing
+const db = require("./models");
+
+// Set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static Directory
+app.use(express.static("public"));
+
+// API Route
+
+require("./routes/api-routes");
+
+//Sync sequelize models and start the Express app
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT" + PORT);
+  });
+});
