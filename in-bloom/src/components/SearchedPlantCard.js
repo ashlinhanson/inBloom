2c68afaad4fa5185360f72a576af81f8b012ab99
ashlinhanson <ashlinhanson@gmail.com>
2020-12-14 21:56:50 -0600

    framing out some components

--- in-bloom/src/components/SearchedPlantCard.js
+++ in-bloom/src/components/SearchedPlantCard.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function SearchedPlantCard() {
+ return (
 
+    <div className="card">
+        <button className="btn btn-success">Add to you garden</button>
+    </div>
+ )
 
 }
 
