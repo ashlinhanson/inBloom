a609c299a7038348095771c54f3363a2c274660b
naywilkins512 <nathanwilkins01@gmail.com>
2020-12-14 20:37:25 -0600

    adds navbar

--- in-bloom/src/components/NavBar.js
+++ in-bloom/src/components/NavBar.js
@@ -1,8 +1,31 @@
 import React from "react";
+import React, { useEffect } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { gsap, TimelineLite, Power3 } from 'gsap';
 
-function NavBar() {
+function Navbarr() {
+
+    let tl = new TimelineLite({ delay: 0.8 })
+
+    useEffect(() => {
+
+        tl.from('.homes', { x: 15, opacity: 0, ease: Power3.easeOut, delay: 0.1 }, 'Start')
+        tl.from('.about', { x: 15, opacity: 0, ease: Power3.easeOut, delay: 0.2 }, 'Start')
+        tl.from('.contact', { x: 15, opacity: 0, ease: Power3.easeOut, delay: 0.3 }, 'Start')
+    }, []);
 
 
+    return (
+
+        <Navbar className="sub" expand="lg" fixed="top" bg="transparent" variant="light">
+            <Nav defaultActiveKey="/home" className="flex-column mt-3 ml-auto">
+                <Nav.Link className="sub homes" href="/home">H O M E</Nav.Link>
+                <Nav.Link className="sub about" href="/about">A B O U T</Nav.Link>
+                <Nav.Link className="sub contact" href="/contact">C O N T A C T</Nav.Link>
+            </Nav>
+        </Navbar>
+
+    )
 };
 
-export default NavBar;
+export default Navbarr;
