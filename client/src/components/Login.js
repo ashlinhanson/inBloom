4cd16e5eee2f04b92a102c4d8532bc6ca0bb4fac
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-12-17 15:48:17 -0600

    page is finally rendering again

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GoogleLogin } from 'react-google-login';
 import { refreshTokenSetup } from '../utils/refreshToken';
-import controller from "../../../controllers/userController";
 
 const clientId = "21199057526-pc5p89vu1fos35ufcd9m597mmd84aq88.apps.googleusercontent.com";
 
@@ -10,7 +9,6 @@ const clientId = "21199057526-pc5p89vu1fos35ufcd9m597mmd84aq88.apps.googleuserco
 function Login() {
     const onSuccess = (res) => {
         console.log("[Login Success] currentUser:", res.profileObj);
-        controller.findById(res.profileObj.id)
     };
 
     const onFailure = (res) => {
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,28 +4,7 @@ export default {
   searchPlants: function() {
     return axios.get("/search/:plantName");
   },
-  getPlant: function(id) {
-    return axios.get("/api/plants/" + id);
-  },
-//   deleteBook: function(id) {
-//     return axios.delete("/api/books/" + id);
-//   },
-  addPlant: function(plantData) {
-    return axios.post("/api/plants", plantData);
+  plantDetail: function(id) {
+    return axios.get("/plant/" + id);
   }
 };
-
-
-app.get("/search/:plantName", (req, res) => {
-    const plantName = req.params.plantName;
-    const Url = `https://trefle.io/api/v1/plants/search?q=${plantName}/?token=${process.env.TREFFLE_KEY}`;
-    
-    app.get("/garden", (req, res) => {
-        const Url = `https://trefle.io/api/v1/plants?token=${process.env.TREFFLE_KEY}`;
-        
-        app.get("/plant/:id", (req, res) => {
-            const plantID = req.params.id;
-            const Url = `https://trefle.io/api/v1/plants/${plantId}?token=${TREFFLE_KEY}`;
-            
-    
-
