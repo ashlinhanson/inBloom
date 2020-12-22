70b0f6ae0372e3b41bcad59956a3206d36426ee2
naywilkins512 <nathanwilkins01@gmail.com>
2020-12-22 10:37:45 -0600

    client side user state working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import './App.css';
@@ -6,21 +6,39 @@ import PlantsPage from "./pages/Garden";
 import NewUser from "./pages/NewUser";
 
 function App() {
+
+const [user, setUser] = useState(null)
+useEffect(() => {
+  setUser(JSON.parse(sessionStorage.getItem('userData')))
+}, [])
   return (
     <Router>
       <div>
         {/* <Nav /> */}
-        <Switch>
+
+        {/*  if they're not logged in */}
+
+       { !user&& <Switch> 
           <Route exact path={"/"}>
-            <Home />
-          </Route>
-          <Route exact path={"/plants"}>
-            <PlantsPage />
+            <Home setUser={setUser} user={user}/>
           </Route>
           <Route exact path={"/newuser"}>
             <NewUser />
           </Route>
-        </Switch>
+        </Switch>}
+
+        {/* if they're logged in they can access this */}
+
+        {user && <Switch>
+          <Route exact path={"/"}>
+            <Home user={user}/>
+          </Route>
+          <Route exact path={"/plants"}>
+            <PlantsPage />
+          </Route>
+          
+          </Switch>}
+
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,15 +3,28 @@ import ReactDOM from 'react-dom';
 import { GoogleLogin } from 'react-google-login';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import API from '../utils/API';
 // import { refreshTokenSetup } from '../utils/refreshToken';
 
 const clientId = "21199057526-pc5p89vu1fos35ufcd9m597mmd84aq88.apps.googleusercontent.com";
 
 
-function Login() {
+function Login(props) {
+
+
     const onSuccess = (res) => {
         console.log("[Login Success] currentUser:", res.profileObj)
         let userEmail = res.profileObj.email;
+        // without authentication on the backend
+        // make api call to /api/loogin db.user.findone if one doesn't exist with googleid pass profileobj to backend and create a user with google info from state. if there is respond true, don't create new one. 
+        // in callback function from api, run line below (21) to update state
+        props.setUser(res.profileObj);
+
+// this is incorporating the backend
+
+// API.signInUser().then(res=>{
+//     props.setUser(res.profileObj);
+// })
         
     };
 
