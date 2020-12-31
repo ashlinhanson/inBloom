dea61662c1e4442dd20971293123dcf63f732108
Josh Wilson <joshua.wilson6289@gmail.com>
2020-12-31 13:34:06 -0600

    added addPlant method in utils/API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 
 export default {
+  // search by plant name
   searchPlants: function() {
     return axios.get("/search/:plantName");
   },
+
+  // get plant details by id
   plantDetail: function(id) {
     return axios.get("/plant/" + id);
   },
+
+  // add plant
+  addPlant: function(plantData) {
+    return axios.post("/api/plant" + plantData)
+  },
+
+  // find user by email
   findByEmail: function (email) {
     return axios.get()
   },
+
   // post request using userData from google
   signInUser: function (userData) {
     return axios.post("/api/user", userData)
   }
 
+    
 };
