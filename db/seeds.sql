74f2815451b9d01f1661394f94dab07920dba5b2
ashlinhanson <ashlinhanson@gmail.com>
2020-12-31 19:43:11 -0600

    got db working and some debounce

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,19 @@
-INSERT INTO Users (id, email, firstName, lastName)
 
-VALUES ('1','joshua.wilson6289@gmail.com', 'Josh', 'Wilson');
-
-INSERT INTO Users (id, email, firstName, lastName)
-VALUES ('2','ashlinhanson@gmail.com', 'Ashli', 'Hanson');
-
-INSERT INTO Users (id, email, firstName, lastName)
-VALUES ('3','nathanwilkins01@gmail.com', 'Nay', 'Wilkins');
-
-INSERT INTO Users (id, email, firstName, lastName)
-VALUES ('4','liliclift@gmail.com', 'Lili', 'Clift');
-
+INSERT INTO Users (id, googleid, email, firstName, lastName)
+VALUES ('1', 'seed1', 'joshua.wilson6289@gmail.com', 'Josh', 'Wilson');
+INSERT INTO Users (id, googleid, email, firstName, lastName)
+VALUES ('2', 'seed2', 'ashlinhanson@gmail.com', 'Ashli', 'Hanson');
+INSERT INTO Users (id, googleid, email, firstName, lastName)
+VALUES ('3', 'seed3', 'nathanwilkins01@gmail.com', 'Nay', 'Wilkins');
+INSERT INTO Users (id, googleid, email, firstName, lastName)
+VALUES ('4', 'seed4', 'liliclift@gmail.com', 'Lili', 'Clift');
 INSERT INTO Plants (id, common_name, image_url, UserId)
 VALUES ('1','Agave', 'agave.com', '1');
-
 INSERT INTO Plants (id, common_name, image_url, UserId)
 VALUES ('2','Brazil Pothos', 'brazilpothos.com', '2');
-
 INSERT INTO Plants (id, common_name, image_url, UserId)
 VALUES ('3','Pothos', 'pothos.com', '3');
-
 INSERT INTO Plants (id, common_name, image_url, UserId)
 VALUES ('4','Basil', 'basil.com', '4');
-
 
 
