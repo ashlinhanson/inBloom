359254a0365aa186af39aff7bf4a5a35e69adaf6
naywilkins512 <nathanwilkins01@gmail.com>
2021-01-05 20:31:24 -0600

    managing state with card search

--- client/src/components/PlantCard.js
+++ client/src/components/PlantCard.js
@@ -6,9 +6,9 @@ function PlantCard(props) {
 
     return(
         <div className="row p-3 mx-auto">
-                <div className="card m-2 p-2 mx-auto" key={props.plant.common_name} style={{width: "350px"}}>
-                    <h3 className="card-title">{props.plant.common_name}</h3>
-                    {/* <img src={props.plant.image_url} className="card-img-top" alt={props.plant.scientific_name} style={{height: "350px"}}/> */}
+                <div className="card m-2 p-2 mx-auto" key={props.common_name} style={{width: "350px"}}>
+                    <h3 className="card-title">{props.common_name}</h3>
+                    <img src={props.image_url} className="card-img-top" alt={props.scientific_name} style={{height: "350px"}}/>
                             <WaterBtn/>
                             <MoreInfoBtn />
                 </div>
--- client/src/pages/Garden.js
+++ client/src/pages/Garden.js
@@ -17,21 +17,23 @@ function Garden(props) {
    //    API.findByEmail(results => {
    //       setPlants({ ...plants, savedPlants: results });
    //   });
-   // };
+   console.log(results)
    return (
       <div>
          <div className="jumbotron bg-success">
             <h1 className="display-3 text-center">Your Garden</h1>
          </div>
-         <Navbar setResults={setResults} /> 
+         <Navbar setResults={setResults} results={results} /> 
          <div className="container p-3 mb-5 rounded text-center mx-auto" style={{width: "fit-content"}}>
         
         {/* trigger a modal with belows results that you can add to the garden */}
          {results.map(result => (
-                    <div key={result.id}>
-                      <h4>{result.common_name}</h4>
+                    <div key={result.plant}>
+                        <PlantCard plant={result.plant}/>
+
+                      
                     </div>
-   // <PlantCard plant={result} />
+  
                
                   ))}
 
