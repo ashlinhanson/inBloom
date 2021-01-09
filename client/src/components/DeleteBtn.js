54a78f1ecde346291154c4e50cedf867787ca4d5
ashlinhanson <ashlinhanson@gmail.com>
2021-01-09 09:52:46 -0600

    attempting the function to remove from the database

--- client/src/components/DeleteBtn.js
+++ client/src/components/DeleteBtn.js
@@ -3,8 +3,13 @@ import React from "react";
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
+
+    function onClick(){
+        mysql_query("DELETE FROM plants WHERE ");
+    }
+
   return (
-    <span className="delete-btn text-right" {...props} role="button" tabIndex="0">
+    <span className="delete-btn text-right" {...props} role="button" tabIndex="0" onclick={onClick}>
       ✗
     </span>
   );
