d114ba2b6974070c030610d83e71886d85c47541
naywilkins512 <nathanwilkins01@gmail.com>
2021-01-10 17:39:04 -0600

    adds ease slowout to animation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React, {useEffect} from "react";
 import Login from "../components/Login";
 import { GoogleLogin } from 'react-google-login';
-import gsap from 'gsap';
+import gsap, {Power1} from 'gsap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import inbloom from "../images/inbloom.png";
 import leftpalm from "../images/leftpalm.svg";
 import rightpalm from "../images/rightpalm.svg";
 import API from '../utils/API';
+
 const clientId = "21199057526-pc5p89vu1fos35ufcd9m597mmd84aq88.apps.googleusercontent.com";
 
 
@@ -37,17 +38,27 @@ function Home(props) {
 
 useEffect(() => {
 
-    gsap.to('.leftpalm-1', { duration: 1, transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-2', { duration: 2, transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-3', { duration: 3, transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-4', { duration: 4, transformOrigin: 'left bottom', rotate: -75 })
+    gsap.from('.inbloom', {duration:4, opacity:0})
+    gsap.to('.leftpalm-1', { duration: 4, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-2', { duration: 3, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-3', { duration: 2, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-4', { duration: 2.2, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-5', { duration: 2.1, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-6', { duration: 2.2, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
 
-    gsap.to('.rightpalm-1', { duration: 1.5, transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-2', { duration: 2.5, transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-3', { duration: 3.5, transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-4', { duration: 4.5, transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-1', { duration: 4.5, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-2', { duration: 3.5, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-3', { duration: 2.5, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-4', { duration: 2.3, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-5', { duration: 2.1, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-6', { duration: 2, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
 },
      []);
+
+     // Hack to hide the render of the SVG
+// requestAnimationFrame(() => {
+//     document.querySelector('.leftpalm-1, .leftpalm-2, .leftpalm-3, .leftpalm-4, .leftpalm-5, .leftpalm-6, .rightpalm-1, .rightpalm-2, .rightpalm-3, .rightpalm-4, .rightpalm-5, .rightpalm-6').style.opacity = 1;
+//   });
 
    
     return(
@@ -59,12 +70,16 @@ useEffect(() => {
             <img className="leftpalm-2" src={leftpalm}></img>
             <img className="leftpalm-3" src={leftpalm}></img>
             <img className="leftpalm-4" src={leftpalm}></img>
+            <img className="leftpalm-5" src={leftpalm}></img>
+            <img className="leftpalm-6" src={leftpalm}></img>
             
             {/* right palm leaves */}
             <img className="rightpalm-1" src={rightpalm}></img>
             <img className="rightpalm-2" src={rightpalm}></img>
             <img className="rightpalm-3" src={rightpalm}></img>
             <img className="rightpalm-4" src={rightpalm}></img>
+            <img className="rightpalm-5" src={rightpalm}></img>
+            <img className="rightpalm-6" src={rightpalm}></img>
 
             {/* the header of the homepage  */}
             <div className="jumbotron bg-transparent text-center">
