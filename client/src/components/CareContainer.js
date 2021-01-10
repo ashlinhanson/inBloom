e07df1beb981bd9c0bf700837c6187892bd2dcaf
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2021-01-10 15:56:22 -0600

    finished care container rendering

--- client/src/components/CareContainer.js
+++ client/src/components/CareContainer.js
@@ -9,7 +9,7 @@ function CareContainer(props) {
         <div>
           <h5>Care Info</h5>
           <ul>
-          {(props.phmin && props.phmax) && `<li>pH Range: ${props.phmin} - ${props.phmax}</li>`}
+          {(props.phmin && props.phmax) && <li>pH Range: {props.phmin} - {props.phmax}</li>}
             {props.light && <li>Light: {props.light}</li>}
             {props.bloommonths && <li>Bloom Months: {props.bloommonths}</li>}
             {props.growthmonths && <li>Growth Months: {props.growthmonths}</li>}
@@ -18,9 +18,9 @@ function CareContainer(props) {
             {props.rootdepth && <li>Minimum Root Depth: {props.rootdepth}cm</li>}
             {(props.mintemp && props.maxtemp) && <li>Temperature Range: {props.mintemp}ºF - {props.maxtemp}ºF</li>}
             {props.soilnutriments && <li>Soil Nutriments: {props.soilnutriments} (pH Level)</li>}
-            {props.soilsalinity && `<li>Soil Salinity: ${props.soilsalinity}</li>`}
-            {props.soiltexture && `<li>Soil Texture: ${props.soiltexture}</li>`}
-            {props.soilhumidity && `<li>Soil Humidity: ${props.soilhumidity}</li>`}
+            {props.soilsalinity && <li>Soil Salinity: {props.soilsalinity}</li>}
+            {props.soiltexture && <li>Soil Texture: {props.soiltexture}</li>}
+            {props.soilhumidity && <li>Soil Humidity: {props.soilhumidity}</li>}
           </ul>
         </div>
       )}
