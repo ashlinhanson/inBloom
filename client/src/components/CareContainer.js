f0c3a3f9c5a1561bd766291fb6b928a402fa75c8
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2021-01-10 15:55:21 -0600

    temp and fertilizer

--- client/src/components/CareContainer.js
+++ client/src/components/CareContainer.js
@@ -16,8 +16,8 @@ function CareContainer(props) {
             {props.humidity && <li>Humidity: {props.humidity}</li>}
             {(props.minwater && props.maxwater) && <li>Precipitation Range: {props.minwater}mm - {props.maxwater}mm</li>}
             {props.rootdepth && <li>Minimum Root Depth: {props.rootdepth}cm</li>}
-            {(props.mintemp && props.maxtemp) && `<li>Temperature Range: ${props.mintemp}ºF - ${props.maxtemp}ºF</li>`}
-            {props.soilnutriments && `<li>Soil Nutriments: ${props.soilnutriments} (pH Level)</li>`}
+            {(props.mintemp && props.maxtemp) && <li>Temperature Range: {props.mintemp}ºF - {props.maxtemp}ºF</li>}
+            {props.soilnutriments && <li>Soil Nutriments: {props.soilnutriments} (pH Level)</li>}
             {props.soilsalinity && `<li>Soil Salinity: ${props.soilsalinity}</li>`}
             {props.soiltexture && `<li>Soil Texture: ${props.soiltexture}</li>`}
             {props.soilhumidity && `<li>Soil Humidity: ${props.soilhumidity}</li>`}
