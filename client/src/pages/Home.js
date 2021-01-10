84d472cd10f4ac9db7a4af8fbb635525b89f1c36
naywilkins512 <nathanwilkins01@gmail.com>
2021-01-10 14:14:04 -0600

    comments out css animation insterts gsap animation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Login from "../components/Login";
 import { GoogleLogin } from 'react-google-login';
+import gsap from 'gsap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import inbloom from "../images/inbloom.png";
 import leftpalm from "../images/leftpalm.svg";
@@ -31,6 +32,23 @@ function Home(props) {
         console.log("[Login failed] res: ", res);
         alert("invalid user");
     };
+
+// gsap animation
+
+useEffect(() => {
+
+    gsap.to('.leftpalm-1', { duration: 1, transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-2', { duration: 2, transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-3', { duration: 3, transformOrigin: 'left bottom', rotate: -75 })
+    gsap.to('.leftpalm-4', { duration: 4, transformOrigin: 'left bottom', rotate: -75 })
+
+    gsap.to('.rightpalm-1', { duration: 1.5, transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-2', { duration: 2.5, transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-3', { duration: 3.5, transformOrigin: 'right bottom', rotate: 75 })
+    gsap.to('.rightpalm-4', { duration: 4.5, transformOrigin: 'right bottom', rotate: 75 })
+},
+     []);
+
    
     return(
      
@@ -39,10 +57,14 @@ function Home(props) {
             {/* left palm leaves */}
             <img className="leftpalm-1" src={leftpalm}></img>
             <img className="leftpalm-2" src={leftpalm}></img>
-
+            <img className="leftpalm-3" src={leftpalm}></img>
+            <img className="leftpalm-4" src={leftpalm}></img>
+            
             {/* right palm leaves */}
             <img className="rightpalm-1" src={rightpalm}></img>
             <img className="rightpalm-2" src={rightpalm}></img>
+            <img className="rightpalm-3" src={rightpalm}></img>
+            <img className="rightpalm-4" src={rightpalm}></img>
 
             {/* the header of the homepage  */}
             <div className="jumbotron bg-transparent text-center">
