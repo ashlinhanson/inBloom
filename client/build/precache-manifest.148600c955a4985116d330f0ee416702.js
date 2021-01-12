31817d0c9613547e214d9729a712a647dccbd3fb
ashlinhanson <ashlinhanson@gmail.com>
2021-01-12 17:46:21 -0600

    most recent build with all functionality working

---
+++ client/build/precache-manifest.148600c955a4985116d330f0ee416702.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e087fa3f15244b1eb7875e829135d3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d7120acbe852bc5883e",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cf58735f12f1f835d152",
+    "url": "/static/css/main.653ed3f4.chunk.css"
+  },
+  {
+    "revision": "2d7120acbe852bc5883e",
+    "url": "/static/js/2.e9f83a92.chunk.js"
+  },
+  {
+    "revision": "3df002cf00bbb36ee45bacdd401fe26c",
+    "url": "/static/js/2.e9f83a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf58735f12f1f835d152",
+    "url": "/static/js/main.daf347f3.chunk.js"
+  },
+  {
+    "revision": "b94b8238fdaf8ff319f7",
+    "url": "/static/js/runtime-main.e3ef4e1f.js"
+  },
+  {
+    "revision": "7ef5483a2ea98d393d5ab448245db68e",
+    "url": "/static/media/flower.7ef5483a.png"
+  },
+  {
+    "revision": "16a4601da40a6894d0cfd8604961fc35",
+    "url": "/static/media/inbloom-background-01.16a4601d.png"
+  },
+  {
+    "revision": "c82f97adf58be76206b3b20d66a77016",
+    "url": "/static/media/inbloom.c82f97ad.png"
+  },
+  {
+    "revision": "0b40382ffe3867522870859e15f357cd",
+    "url": "/static/media/leftpalm.0b40382f.svg"
+  },
+  {
+    "revision": "9e0023e95239affd3b28c62a75a81c35",
+    "url": "/static/media/no-plant.9e0023e9.png"
+  },
+  {
+    "revision": "36c68f8f04954100c3a7afc5605c39d6",
+    "url": "/static/media/plant1.36c68f8f.png"
+  },
+  {
+    "revision": "506a618dacf6692408a47adeffeaa204",
+    "url": "/static/media/plant2.506a618d.png"
+  },
+  {
+    "revision": "4413f5287958bd8477638c0b8fe23f2e",
+    "url": "/static/media/planty.4413f528.jpg"
+  },
+  {
+    "revision": "d27a76c8c11601854dcf476b778b256e",
+    "url": "/static/media/rightpalm.d27a76c8.svg"
+  },
+  {
+    "revision": "94212cfec948b659c5a92d78baeb05f0",
+    "url": "/static/media/yourgarden.94212cfe.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24dcc727ad68e9a057ced6a054989a53.js"
+  "/precache-manifest.148600c955a4985116d330f0ee416702.js"
 );
 
 self.addEventListener('message', (event) => {
