ffdfb61ca99ded223daf1cc37a8ab1d9c3943ecc
naywilkins512 <nathanwilkins01@gmail.com>
2021-01-12 09:58:50 -0600

    made palms a component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,13 @@
 import React, {useEffect} from "react";
 import Login from "../components/Login";
 import { GoogleLogin } from 'react-google-login';
-import gsap, {Power1} from 'gsap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import inbloom from "../images/inbloom.png";
-import leftpalm from "../images/leftpalm.svg";
-import rightpalm from "../images/rightpalm.svg";
 import API from '../utils/API';
 import plant1 from '../images/plant1.png';
 import plant2 from '../images/plant2.png';
 import flower from '../images/flower.png';
+import Palms from '../components/Palms';
 
 const clientId = "21199057526-pc5p89vu1fos35ufcd9m597mmd84aq88.apps.googleusercontent.com";
 
@@ -37,27 +35,6 @@ function Home(props) {
         alert("invalid user");
     };
 
-// gsap animation
-
-useEffect(() => {
-
-    gsap.from('.inbloom', {duration:4, opacity:0})
-    gsap.from('#login-container', {duration:4, opacity:0})
-    gsap.to('.leftpalm-1', { duration: 4, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-2', { duration: 3, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-3', { duration: 2, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-4', { duration: 2.2, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-5', { duration: 2.1, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
-    gsap.to('.leftpalm-6', { duration: 2.2, ease: "slow.out", transformOrigin: 'left bottom', rotate: -75 })
-
-    gsap.to('.rightpalm-1', { duration: 4.5, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-2', { duration: 3.5, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-3', { duration: 2.5, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-4', { duration: 2.3, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-5', { duration: 2.1, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
-    gsap.to('.rightpalm-6', { duration: 2, ease: "slow.out", transformOrigin: 'right bottom', rotate: 75 })
-},
-     []);
 
      // Hack to hide the render of the SVG
 // requestAnimationFrame(() => {
@@ -69,21 +46,7 @@ useEffect(() => {
      
         <div>
 
-            {/* left palm leaves */}
-            <img className="leftpalm-1" src={leftpalm}></img>
-            <img className="leftpalm-2" src={leftpalm}></img>
-            <img className="leftpalm-3" src={leftpalm}></img>
-            <img className="leftpalm-4" src={leftpalm}></img>
-            <img className="leftpalm-5" src={leftpalm}></img>
-            <img className="leftpalm-6" src={leftpalm}></img>
-            
-            {/* right palm leaves */}
-            <img className="rightpalm-1" src={rightpalm}></img>
-            <img className="rightpalm-2" src={rightpalm}></img>
-            <img className="rightpalm-3" src={rightpalm}></img>
-            <img className="rightpalm-4" src={rightpalm}></img>
-            <img className="rightpalm-5" src={rightpalm}></img>
-            <img className="rightpalm-6" src={rightpalm}></img>
+        <Palms/>
 
             {/* the header of the homepage  */}
             <div className="jumbotron bg-transparent text-center">
